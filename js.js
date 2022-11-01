db0d21adb490a3d46fb6f82d58fa23528aa94808
alicerubes <argirling@gmail.com>
2022-11-01 13:24:51 +0100

    Contact form
    Couldn't make the popup form work how I wanted it to so just added it directly onto the page.

--- js.js
+++ js.js
@@ -1,21 +1,10 @@
-// POPUP CONTACT FORM START
 
-// Validating Empty Field
-function check_empty() {
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-    alert("Please Fill All Fields !");
-    } else {
-    document.getElementById('form').submit();
-    alert("Form submitted successfully. Thanks for reaching out !");
-    }
-}
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-  
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+// POPUP PRIVACY POLICY START
 
-// POPUP CONTACT FORM END
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+// POPUP PRIVACY POLICY END
