e3646da073c8f344499b5aa6254fa8d52de16431
alicerubes <argirling@gmail.com>
2022-11-07 15:44:08 +0100

    Completed my page

--- js.js
+++ js.js
@@ -1,10 +1,30 @@
+// Alice page START
 
+var testi = $('#testimonial-slide');
+var owlcarouselslide = function(e) {
+   
+    // testimonial slide
+    testi.owlCarousel({
+        autoplay: false,
+        center: true,
+        loop: true,
+        dots: true,
+        touchDrag: false,
+        mouseDrag: false,
+        smartSpeed: 900,
+        items: 1,
+        margin: 100,
+        nav: true,
+        navText: [
+            '<i class="fa fa-long-arrow-left " aria-hidden="true"></i>',
+            '<i class="fa fa-long-arrow-right " aria-hidden="true"></i>'
+        ],
+        navContainer: '.custom-nav',
+    });
+};
 
-// POPUP PRIVACY POLICY START
+$(document).ready(function() {
+    owlcarouselslide();
+});
 
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
-
-// POPUP PRIVACY POLICY END
+// Alice page END
